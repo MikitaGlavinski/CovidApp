4abcc28a28a2081c0173ba912b09e6480afe2138
Mikita Glavinski <Mikita.Glavinski@itechart-group.com>
2021-11-22 16:35:45 +0300

    initial

---
+++ CovidView/CovidView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidView
+//
+//  Created by Mikita Glavinski on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
